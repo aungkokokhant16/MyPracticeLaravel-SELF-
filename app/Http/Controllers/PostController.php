58f4b09f4820f405a1e4859bd0ef9394cce544ea
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2021-09-20 22:45:32 +0630

    CRUD Practice (Two ways CRUD)

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+
+    {
+        //create
+        // $post = new Post();
+        // $post->title = "this is title";
+        // $post->description = "this is description";
+        // $post->save();
+
+        // Post::create([
+        //     'title' => 'my practice',
+        //     'description' => 'my web practice'
+        // ]);
+        // return "success";
+
+        //update
+        // Post::where('id', 2)->update([
+        //     'title' => 'my design',
+        //     'description' => 'my learning'
+        // ]);
+
+        // $post = Post::find(2);
+        // $post->title = "this is my first practice";
+        // $post->description = "this is my first crud";
+        // $post->save();
+
+        //Delete
+        // Post::where('id', 2)->delete();
+
+        $post = Post::find(1);
+        $post->delete();
+    }
+}
